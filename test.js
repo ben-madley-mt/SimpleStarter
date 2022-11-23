86f900a934c9393786c6b535db89ff4304c68a4a
ben-madley-mt <ben.madley@madetech.com>
2022-11-23 10:00:36 +0000

    Extract a small client.

--- test.js
+++ test.js
@@ -1,24 +1,5 @@
 const axios = require('axios')
-
-const createNewCard = async (title, body) => {
-    const response = await axios.post('http://localhost:3000/card', { title: title, body: body})
-    return response.data.id
-}
-
-const updateCard = async (id, title, body) => {
-    const response = await axios.put(`http://localhost:3000/card/${id}`, { title: title, body: body})
-    return response.data.changes > 0
-}
-
-const getCard = async (id) => {
-    const response = await axios.get(`http://localhost:3000/card/${id}`)
-    return response.data
-}
-
-const deleteCard = async (id) => {
-    const response = await axios.delete(`http://localhost:3000/card/${id}`)
-    return response.data.changes > 0
-}
+const {createNewCard, updateCard, getCard, deleteCard} = require("./client/client");
 
 const doACard = async () => {
     const id = await createNewCard('title', 'body')
