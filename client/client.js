acaed0bf8cdd5070a1e02cd64f7a6a3dd1e8d6ae
ben-madley-mt <ben.madley@madetech.com>
2022-11-23 10:42:04 +0000

    Allow specifying the api url.

--- client/client.js
+++ client/client.js
@@ -1,23 +1,29 @@
 const axios = require('axios')
 
 class CardClient {
+    url;
+
+    constructor(url = 'http://localhost:3000') {
+        this.url = url;
+    }
+
     async createNewCard(title, body) {
-        const response = await axios.post('http://localhost:3000/card', {title: title, body: body})
+        const response = await axios.post(`${this.url}/card`, {title: title, body: body})
         return response.data.id
     }
 
     async updateCard(id, title, body) {
-        const response = await axios.put(`http://localhost:3000/card/${id}`, {title: title, body: body})
+        const response = await axios.put(`${this.url}/card/${id}`, {title: title, body: body})
         return response.data.changes > 0
     }
 
     async getCard(id) {
-        const response = await axios.get(`http://localhost:3000/card/${id}`)
+        const response = await axios.get(`${this.url}/card/${id}`)
         return response.data
     }
 
     async deleteCard(id) {
-        const response = await axios.delete(`http://localhost:3000/card/${id}`)
+        const response = await axios.delete(`${this.url}/card/${id}`)
         return response.data.changes > 0
     }
 }
